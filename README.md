7bdf36a4ae62610b76734dedb5e8af51dcab9b3f
v.khadyev <v.khadyev@tinkoff.ru>
2023-11-08 20:37:37 +0500

    Done

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # qa_auto_hw_2
+
+## Автоформатеры
+* black - `black .`
+
+## Линтеры
+* flake8 - `pflake8`
+* isort - `isort .`
---
+++ app/main.py
@@ -0,0 +1,11 @@
+import decimal
+import math
+
+
+def main():
+    name = decimal.Decimal(math.pi)
+    print(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    main()
